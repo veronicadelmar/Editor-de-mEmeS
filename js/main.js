bb84415b177bce5ca03a57bf564c5549900476e9
Veronica Guiñan <veronicadmgl@gmail.com>
2023-03-12 15:19:40 -0300

    Modified event input url with url image and event input color bg

--- js/main.js
+++ js/main.js
@@ -3,6 +3,7 @@ const $ = (selector) => document.querySelector(selector)
 const num = 5
 
 // getAttribute, setAttribute, removeAttribute
+// _________ HEADER _________
 // Dark button - Mode change
 $("#btn__dark").addEventListener("click", () =>{
     const current__dark = $("body").getAttribute("data-theme")
@@ -29,9 +30,13 @@ $("#btn__light").addEventListener("click", () =>{
         $("body").setAttribute("data-theme", "light__theme")
     }
 })
+// BTN - Image and Text
 // _________ ASIDE - Image panel - appear ________
 $("#btn__img").addEventListener("click", () =>{
     $(".image__panel").classList.remove("hidden")
+    if ($(".image__panel").style.display = "block"){
+        $(".text__panel").style.display = "none"
+    }
 })
 // Image panel - disappear
 $(".btn__imagen__panel--close").addEventListener("click", () =>{
@@ -40,8 +45,30 @@ $(".btn__imagen__panel--close").addEventListener("click", () =>{
 // _________ ASIDE - Text panel - appear ________
 $("#btn__text").addEventListener("click", () =>{
     $(".text__panel").classList.remove("hidden")
+    if ($(".text__panel").style.display = "block"){
+        $(".image__panel").style.display = "none"
+    }
 })
 // Text panel - disappear
 $(".btn__text__panel--close").addEventListener("click", () =>{
     $(".text__panel").classList.add("hidden")
 })
+// _________ MAIN _________
+// IMAGEN URL
+$("#url__image").addEventListener("input", () =>{
+    $("#image__meme").src = $("#url__image").value
+    $("#image__meme").style.width = "100%" 
+    $("#image__meme").style.height = "100%"
+})
+// _________ ASIDE - Image panel
+// Background
+$("#background").addEventListener("input", () =>{
+    $("figure").style.backgroundColor = $("#background").value
+    $("span").innerText = ($("#background").value).toUpperCase()
+    $("#background").style.backgroundColor = $("#background").value
+})
+// Background selector
+$(".bg__select").addEventListener("change", () =>{
+    $("#image__meme").style.backgroundBlendMode = $(".bg").value
+})
+
